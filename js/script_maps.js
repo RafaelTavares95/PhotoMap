46a3fe27bc7bd4425c422b83280d064bf185d236
Rafael_Tavares <rafaeltavares.rofino198@gmail.com>
2015-05-20 03:45:19 -0300

    Adicionando coisas

--- js/script_maps.js
+++ js/script_maps.js
@@ -1,32 +1,32 @@
 function initialize() {
-	  
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  
-  google.maps.event.addListener(map, 'click', function(event){
-	placeMarker(event.latLng);
-  });
-  
-  function placeMarker(location){
-	marker = new google.maps.Marker({
-		position: location,
-		map: map,
-		title: 'Novo Marcador',
-		animation: google.maps.Animation.DROP
-	});
-	map.setCenter(location);
-  }
-}
+		var mapOptions = {
+			zoom: 12,
+			center: new google.maps.LatLng(-25.363882,131.044922),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+		google.maps.event.addListener(map, 'click', function(event) {
+    		placeMarker(event.latLng);
+  		});
 
-function loadScript() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBQPmnXCrhTYYJdESzaHRh7e3qeendXXAs&sensor=TRUE&callback=initialize";
-  document.body.appendChild(script);
-}
+  		function placeMarker(location) {
+		var marker = new google.maps.Marker({
+			position: location,
+			map: map,
+			title: "new Marker",
+			animation: google.maps.Animation.DROP
+		});
 
-window.onload = loadScript;
+		$('#latitude').val(location.lat())
+		$('#longitude').val(location.lng())
+		map.setCenter(location);
+	}
+	}
+
+
+	function loadScript() {
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyChZmr53ae-Rp57sXbnHgKw-H9L0FGyHbw&sensor=TRUE_OR_FALSE&callback=initialize";
+		document.body.appendChild(script);
+	}
